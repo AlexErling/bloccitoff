d9df099e72fab6a7daf8c3ff74d39cdce4c80009
Alex Erling <alex.erling@gmail.com>
2017-11-01 19:34:55 +0000

    Display time and delete expired items done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
  require 'faker'
  
+  User.create!(
+     email: 'alex.erling@gmail.com',
+     password: 'password'
+ )
+ 
  10.times do
      Item.create!(
          name: Faker::Lorem.sentence,
+         created_at: 3.days.ago
      )
  end
  
- User.create!(
-     email: 'alex.erling@gmail.com',
-     password: 'helloworld'
- )
- 
+  10.times do
+     Item.create!(
+         name: Faker::Lorem.sentence,
+         created_at: 2.months.ago
+     )
+ end
+
  puts "Seed finished"
  puts "#{Item.count} items created"
  puts "#{User.count} users created" 
